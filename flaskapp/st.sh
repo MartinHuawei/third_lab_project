7280c94e11e3aeaf63a394fb70b096df40a399cf
martin_huawei <di.shef70@gmail.com>
2022-06-12 19:12:51 +0700

    Update python-app.yml

--- flaskapp/st.sh
+++ flaskapp/st.sh
@@ -1,9 +1,10 @@
 gunicorn --bind 127.0.0.1:5000 wsgi:app & APP_PID=$!
+
 sleep 5
 
 python3 some_app.py
+CLIENT_RETURNS=$?
 
 echo $APP_PID
 kill -TERM $APP_PID
-echo process gunicorns kills
-exit 0
+exit $CLIENT_RETURNS
